d8e18beec38f16d453f6809b28827d3398fa9dc8
Evgeny <kurakin.evgeny@gmail.com>
2021-08-16 12:26:23 +0300

    Домашняя работа 1

---
+++ react-homework/src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const element = <h1>Hello World меня зовут: Евгений Куракин</h1>
+
+ReactDOM.render(element, document.getElementById("root"))
